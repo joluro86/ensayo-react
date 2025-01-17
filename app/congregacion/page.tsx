a2deb4d6c5c133d8b604665dfd35db97d242df0a
joluro86 <joluro86@hotmail.com>
2025-01-17 15:25:08 -0500

    ensayo1

---
+++ app/congregacion/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const congregaciones = () => {
+  return (
+    <div>congregaciones</div>
+  )
+}
+
+export default congregaciones
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Image from "next/image";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +24,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="es">
+      <body className="">
+        <div className="">
+          <nav className="bg-blue-200">
+          </nav>
+          <main className="bg-blue-500">
+          </main>
+          {children}
+          <footer className="bg-blue-400 row-start-3 text-xs flex gap-6 flex-wrap items-center justify-center">
+            Diseñado por Joluro Soft - Todos los derechos reservados
+          </footer>
+        </div>
       </body>
     </html>
   );
